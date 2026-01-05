c74d8957c16e9979b66befc58f80e511715b02e2
zhangqizheng <zhangqizheng@qimingpian.com>
2026-01-05 09:12:25 +0800

    base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -36,7 +36,7 @@ import rehypeFigure from "./src/plugins/rehype-figure.mjs";
 export default defineConfig({
 	site: siteConfig.site_url,
 
-	base: "/",
+	base: "/The-Moments-Between",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
